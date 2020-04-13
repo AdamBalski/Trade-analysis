951a71fd572b4eb0ac8f5a500f167cd6d3b4a1ad
Adam Balski <adambalski1@gmail.com>
2020-04-13 20:00:39 +0200

    First commit.

---
+++ src/main/java/com/trade_analysis/Trade/Analysis/TradeAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.trade_analysis.Trade.Analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trade_analysis/Trade/Analysis/TradeAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade_analysis.Trade.Analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
